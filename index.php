15118c3451d6e377222cd09790daad02b105d094
Feri Irawan <57158078+feri-irawan@users.noreply.github.com>
2021-04-14 16:28:17 +0800

    update index.php

--- index.php
+++ index.php
@@ -2,3 +2,9 @@
 include_once("https://blog-lokal.000webhostapp.com/connect.php");
 
 $conn = sql_connect("localhost", "id15353728_root2", "@Qazxcvbn123", "id15353728_login");
+if (!mysqli_connect_errno()) {
+  echo("Success");
+} else {
+  echo("Gagagl");
+}
+?>
