27c94dd0d40777f9e7fe7c8b40fd6504b91dcc3c
Feri Irawan <57158078+feri-irawan@users.noreply.github.com>
2021-04-14 14:41:11 +0800

    update index.php

--- index.php
+++ index.php
@@ -1,19 +1,3 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title>Document</title>
-  
-  <link rel="stylesheet" href="https://cdn.statically.io/gh/feri-irawan/Flix/main/dist/flix.css" type="text/css" media="all" />
-</head>
-<body>
-  
-  <div class="bg-orange p-20 ff-monospace">
-    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iste labore deleniti architecto eos. Repellendus suscipit ducimus vel consequatur praesentium quibusdam id, magnam, aut. Voluptates suscipit ratione fugiat cum animi provident!
-  </div>
-  
-  
-</body>
-</html>
+<?php
+print("hello world");
+?>
