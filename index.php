f7bbc624ab6cf3ebc787954d981a6619dea867ca
Feri Irawan <57158078+feri-irawan@users.noreply.github.com>
2021-04-14 15:13:01 +0800

    update index.php

--- index.php
+++ index.php
@@ -1,3 +1,13 @@
 <?php
-print("hello world");
+$host = "https://databases.000webhost.com/";
+$uname = "id15353728_root2";
+$pass = "@Qazxcvbn123";
+$dbname = "id15353728_login";
+
+$conn = mysqli_connect($host, $uname, $pass, $dbname);
+
+if (mysqli_connect_errno()) {
+  echo "Failed to connect to MySQL: " . mysqli_connect_error();
+  }
+
 ?>
