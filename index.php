39455c543368373e6f3b9a67e283b6f12e8d023d
Feri Irawan <57158078+feri-irawan@users.noreply.github.com>
2021-05-14 15:12:13 +0800

    update index.php

--- index.php
+++ index.php
@@ -8,7 +8,7 @@ $title = "Menghitung Selisih Umur";
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title><?=$webName ."-". $title?></title>
+  <title><?=$webName ." - ". $title?></title>
 
 <!-- CSS -->
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
