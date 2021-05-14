32798c5b48249ba3853809740ac2e10988cffcc9
Feri Irawan <57158078+feri-irawan@users.noreply.github.com>
2021-05-14 14:22:38 +0800

    update index.php

--- index.php
+++ index.php
@@ -1,10 +1,14 @@
+<?php
+$webName = "FICODE";
+$title = "Menghitung Selisih Umur";
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title>Document</title>
+  <title><?=$webName ."-". $title?></title>
 
 <!-- CSS -->
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
@@ -200,7 +204,7 @@ $("#html").val(`<!DOCTYPE html>
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet">
-  <title>Menghitung Selisih Umur</title>
+  <title><?=$title?></title>
 </head>
 <body>
 <div class="container p-3">
